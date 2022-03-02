f6a130602893ddcf81cb599adc24f774828baada
Panindra Reddy B <100305087+PANINDRA-REDDY-BANDIATMAKUR@users.noreply.github.com>
2022-03-02 21:27:02 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,16 @@
 #include <vector>
 
+typedef struct EmailAlert 
+{
+    char emailSent;
+    char emailNotSent;
+};
+ 
+typedef struct LEDAlert 
+{
+    char ledGlows;
+    char ledOff;
+};
 namespace Statistics {
     auto ComputeStatistics(const std::vector<int*>& alerters );
 }
